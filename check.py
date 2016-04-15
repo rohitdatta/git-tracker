c4950802bf955b5a848fb09fbab56ad341b1dc15
Rohit Datta <rohitdatta11@gmail.com>
2016-04-14 22:24:12 -0500

    get the git streak on the backend

--- check.py
+++ check.py
@@ -3,6 +3,7 @@ import urllib2
 import requests
 
 from flask import Flask, render_template, jsonify
+from bs4 import BeautifulSoup
 
 app = Flask(__name__)
 #host = os.environ.get('HOST')
@@ -14,6 +15,18 @@ def getGitHubActivity(github_username):
 @app.route('/')
 def index():
 	return render_template('index.html', host=host)
+
+def get_streak(username):
+	r = requests.get('https://github.com/'+username)
+	soup = BeautifulSoup(r.text)
+	contrib_nums = [contrib.text for contrib in soup.findAll('span', attrs={'class': 'contrib-number'})]
+	return contrib_nums[-1]
+
+@app.route('/<username>')
+def get_info(username):
+	streak = get_streak(username)
+#	commits = get_commits(username)
+	return render_template('results.html', streak=streak)
 
 @app.route('/github/<github_username>')
 def github_content(github_username):
