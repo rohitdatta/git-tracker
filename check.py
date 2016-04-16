02d7a772090addf0d1e07d85ee52b063fba7a5d1
Rohit Datta <rohitdatta11@gmail.com>
2016-04-16 13:07:50 -0500

    cleaned up old python code

--- check.py
+++ check.py
@@ -8,8 +8,6 @@ from lxml import html
 from lxml import etree
 
 app = Flask(__name__)
-#host = os.environ.get('HOST')
-host = 'http://127.0.0.1:5000'
 
 def get_custom_message(streak):
 	start_day = date(2016, 4, 7)
@@ -22,13 +20,8 @@ def get_custom_message(streak):
 		else:
 			return 'Our automated check isn\'t able to verify your completion towards a 30 day streak. If you believe this is a mistake, check to make sure all the repositories you committed to are public. If you still think there\'s an error, please reach out to <a href="mailto:hello@freetailhackers.com">hello@freetailhackers.com</a> so we can investigate further.'
 
-def getGitHubActivity(github_username):
-	return json.load(urllib2.urlopen('https://api.github.com/users/'+github_username+'/events/public'))
-
 @app.route('/')
 def index():
-#	if request.method == 'POST':
-#		return get_info(username)
 	return render_template('index.html', host=host)
 
 def get_streak(username, page_tree):
@@ -64,19 +57,9 @@ def get_info(username):
 	if page.status_code == 400:
 		return render_template('404.html')
 	page_tree = html.fromstring(page.content)
-
 	commit_keys, commit_dict, streak = get_commits(username, page_tree)
 	message = get_custom_message(streak)
 	return render_template('results.html', streak=streak, commits=commit_dict, keys=commit_keys, message=message)
-
-@app.route('/github/<github_username>')
-def github_content(github_username):
-	return requests.get('https://github.com/'+github_username).content
-
-@app.route('/api/<github_username>')
-def github_username(github_username):
-	activity = getGitHubActivity(github_username)
-	return jsonify(results=activity)
 
 if __name__ == '__main__':
 	app.run(debug=True)
