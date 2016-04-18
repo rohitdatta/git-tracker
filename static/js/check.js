878301d5a6a9df237f661c63d8ac50c213d06eb6
Rohit Datta <rohitdatta11@gmail.com>
2016-04-18 04:16:15 -0500

    turn a button into a loading icon after submitting

--- static/js/check.js
+++ static/js/check.js
@@ -1,9 +1,6 @@
 $(function() {
     $('#check').on('submit', function(e) {
-		$('body').find('#submit-button').fadeOut(500, function() {
-        $('body').find('#submit-button').html('<i class="fa fa-circle-o-notch fa-spin fa-fw margin-bottom"></i>').fadeIn(500);
-    });
-		
-		
+		$('body').find('#submit-button').addClass('submitted');
+		$('body').find('#submit-button').html('<div class="loader">Loading...</div>');
     });
 });
