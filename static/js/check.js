fbe50a3b61d248fc50170870b48a78067e4214ec
Rohit Datta <rohitdatta11@gmail.com>
2016-04-18 17:12:33 -0500

    error checking because safari doesn't like to require

--- static/js/check.js
+++ static/js/check.js
@@ -1,6 +1,11 @@
 $(function() {
     $('#check').on('submit', function(e) {
-		$('body').find('#submit-button').addClass('submitted');
-		$('body').find('#submit-button').html('<div class="loader">Loading...</div>');
+		if(!$('input[name="username"]').val()) {
+			sweetAlert('Empty Username', 'Please enter your GitHub username', 'error');
+			e.preventDefault();
+		} else {
+			$('body').find('#submit-button').addClass('submitted');
+			$('body').find('#submit-button').html('<div class="loader">Loading...</div>');
+		}
     });
 });
